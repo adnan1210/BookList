43097e6cc75893e04a00ee824bc4ce2727cc7b40
adnan1210 <adnanahsan.1210@gmail.com>
2022-06-17 20:08:33 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const year = document.querySelector("#year");
+const btn = document.querySelector(".btn");
+const tableList = document.querySelector("#book-list");
+
+btn.addEventListener('click' , function(e){
+    e.preventDefault();
+    if(title.value== '' || author.value== '' || year== ''){
+        alert('Please fill the fields and try again')
+    }else{
+        const newRow = document.createElement('tr');
+
+        const newTitle = document.createElement('th');
+        newTitle.innerHTML = title.value;
+        newRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement('th');
+        newAuthor.innerHTML = author.value;
+        newRow. appendChild(newAuthor);
+
+        const newYear = document.createElement('th');
+        newYear.innerHTML = year.value;
+        newRow.appendChild(newYear);
+
+        tableList.appendChild(newRow);
+
+        const inputs = document.querySelectorAll('#title, #author, #year');
+
+        inputs.forEach(input=>{
+            input.value = '';
+        });
+    }
+});
